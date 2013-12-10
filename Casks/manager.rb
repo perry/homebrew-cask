f7e34a079b0c0ff3e767d30fb957259654dc78a9
Perry Roper <perryroper@gmail.com>
2013-12-10 19:33:06 +0000

    Update version of Manager.app

--- Casks/manager.rb
+++ Casks/manager.rb
@@ -1,7 +1,7 @@
 class Manager < Cask
-  url 'http://cdn.manager.io/downloads/v13.10.5040.17151/Manager.dmg'
+  url 'http://cdn.manager.io/downloads/v13.11.5073.14603/Manager.dmg'
   homepage 'http://manager.io'
-  version '13.10.5040.17151'
-  sha1 '3de9d92fc7a6673c0b84bcad652f7f781218f537'
+  version '13.11.5073.14603'
+  sha1 'a63a31c06e963e48e7c1ec6568b741b7aa5c5168'
   link 'Manager.app'
 end
